c1f1fc82f53fa39e2273b52619fe1535de2a996b
user1 <user1@qq.com>
2019-05-07 12:53:14 +0800

    jmyj

---
+++ src/main/java/cn/lyq/GithubTest.java
@@ -0,0 +1,11 @@
+package cn.lyq;
+
+/**
+ * @author lyq
+ * @date 2019/5/7 11:43
+ */
+public class GithubTest {
+    public static void main(String[] args) {
+        System.out.println("用来测试的");
+    }
+}
